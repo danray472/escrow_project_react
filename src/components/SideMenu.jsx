23ef229027d3ad5219e04136be4191d3de1b0dc9
Savin Osuka <osukasavin2001@gmail.com>
2022-11-11 16:13:55 +0300

    test 2

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -9,19 +9,19 @@ const SideMenu = () => {
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 overflow-y-auto sm:w-1/2 md:w-80 bg-base-100 sticky">
           <li>
-            <Link to={`/dashboard`}>Dashboard</Link>
+            <Link to={`/${auth.currentUser.uid}/dashboard`}>Dashboard</Link>
           </li>
           <li>
-            <Link to={`/tasks`}>Tasks</Link>
+            <Link to={`/${auth.currentUser.uid}/tasks`}>Tasks</Link>
           </li>
           <li>
-            <Link to={`/writers`}>Writers</Link>
+            <Link to={`/${auth.currentUser.uid}/writers`}>Writers</Link>
           </li>
           <li>
-            <Link to={`/payments`}>Payments</Link>
+            <Link to={`/${auth.currentUser.uid}/payments`}>Payments</Link>
           </li>
           <li>
-            <Link to={`/profile`}>My Profile</Link>
+            <Link to={`/${auth.currentUser.uid}/profile`}>My Profile</Link>
           </li>
           <li className="bg-error rounded-md text-base-100">
             <span
