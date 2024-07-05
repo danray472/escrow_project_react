018b83d57fcf31fcff3092828581e226b98a480d
dan <@danray472>
2024-07-05 11:34:57 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+    emptyOutDir: true, // Ensure the output directory is cleaned before each build
+  },
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore'], // Include Firebase dependencies for optimization
+  },
+});
